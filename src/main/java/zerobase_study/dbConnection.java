dbdc01995ba0a1e38b56b36f52981c4eb207ebf4
Kim <tldnjs0911@daum.net>
2023-05-29 15:10:46 +0900

    init...

---
+++ src/main/java/zerobase_study/dbConnection.java
@@ -0,0 +1,17 @@
+package zerobase_study;
+
+public class dbConnection {
+
+	public static void main(String[] args) {
+		
+		String url = "jdbc:sqlite://192.168.0.68:8088/Ciwon KiM";
+		
+		String dbUserId = "Ciwon Kim";
+		
+		String dbPassword = "zerobase";
+		
+		
+
+	}
+
+}
